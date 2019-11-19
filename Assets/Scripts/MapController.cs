6df23dff058d12fb8c10ede7cc4cc930a6bc9f77
Rocksar <Rocktar@hotmail.fr>
2019-11-18 23:00:40 -0500

    Les Caméras c'est mon dada

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -7,8 +7,8 @@ public class MapController : MonoBehaviour
     ArrayList tiles=new ArrayList();
     [SerializeField] private Transform Character;
     public GameObject prefabTile;
-    
-    
+    int charj = 1;
+
     void OnGUI()
     {
         //GUI.Box(new Rect(250, 500, 100, 100), "N2");
@@ -17,10 +17,34 @@ public class MapController : MonoBehaviour
     void Start()
     {
         Instantiate(Character, new Vector3(0, 0), Quaternion.identity);
-        for (int i=0; i<5; i++){
-            Tile tile = new Tile();
-            tile.Create(40+150*i,(Screen.height/2)-50,prefabTile);
-            tiles.Add(tile);
+
+        Tile start = new Tile();
+        start.Create(-300,0,0,prefabTile);
+        tiles.Add(start);
+        for (int i=0; i<3; i++)
+        {
+            for (int j=1;j<4;j++)
+            {
+                Tile tile = new Tile();
+                tile.Create(-300+150*j,150*(i-1),j,prefabTile);
+                tiles.Add(tile);
+            }
+        }
+        Tile Boss=new Tile();
+        Boss.Create(300,0,4,prefabTile);
+        tiles.Add(Boss);
+        foreach (Tile i in tiles)
+        {
+            foreach (Tile j in tiles)
+            {
+                // Conditions for linking two different Tiles with one layer
+                if (i.getLayer() == j.getLayer()-1)
+                {
+                    i.Links(j);
+                }
+            }
+            // If not Clickable set low alpha
+            if(charj>=i.getLayer()) i.setAlpha(0.1f);
         }
 
     }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,20 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tile : MonoBehaviour
 {
-    int height;
-    int width;
     GameObject tile;
+    int layer;
+    ArrayList lines=new ArrayList();
+    
     public Tile(){
-        
+
     }
-    public void Create(int x, int y,GameObject prefabTile)
+    public void Create(int x, int y,int layer, GameObject prefabTile)
     {
-        height = x;
-        width = y;
-        tile = Instantiate(prefabTile, new Vector3(height,width),Quaternion.identity);
-        tile.transform.parent = GameObject.Find("Canvas").GetComponent<RectTransform>().transform;
+        this.layer=layer;
+        tile = Instantiate(prefabTile, new Vector3(x,y),Quaternion.identity);
+        tile.transform.SetParent(GameObject.Find("Canvas").GetComponent<RectTransform>().transform,false);
+    }
+    public void Links(Tile t){
+        GameObject line = new GameObject();
+        line.transform.SetParent(GameObject.Find("Canvas").GetComponent<RectTransform>().transform,false);
+        LineRenderer lr = line.AddComponent<LineRenderer>();
+        lr.material = new Material (Shader.Find ("Sprites/Default"));
+        lr.material.color = Color.red;
+        lr.widthMultiplier = 10f;
+        lr.sortingOrder=1;
+        lr.SetPosition(0,tile.transform.position);
+        lr.SetPosition(1,t.tile.transform.position);
+        lines.Add(lr);
+    }
+    public void setAlpha(float alpha){
+        Color tempColor = tile.GetComponentInChildren<Image>().color;
+        tempColor.a = alpha;
+        tile.GetComponentInChildren<Image>().color = tempColor;
+    }
+    public int getLayer(){
+        return layer;
     }
 }
